07596506d05e047fe475446d96ae7553c5eb71c6
him-052005 <himanshumishra19dec@gmail.com>
2025-02-12 10:18:38 +0530

    12th feb task

---
+++ CE/HTTP/http-plain1.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs =require("fs/promises")
+
+const server =  http.createServer(async (req, res) => {
+    console.log(req.url);
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/html');
+   res.write("<H1>Hello, worldddddddddddddd!</H1>");
+   const data = await fs.readFile("./http.html", "utf-8");
+   res.end(data);
+       
+});
+PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
