95053e3835c7fb0e058f9e33132bbd916fdd27bd
him-052005 <himanshumishra19dec@gmail.com>
2025-02-24 10:15:58 +0530

    this is 24th feb work

--- CE/HTTP/Express/express-intro.js
+++ CE/HTTP/Express/express-intro.js
@@ -3,6 +3,11 @@ const app=express();
 app.get('/',(request,response)=>{
     response.send("hello ce students");
 });
+app.get('/api',(request,response)=>{
+    const {name='Guest', rollno='100'}=request.query;
+    console.log(data)
+    response.send(`hello dear ${name} and my roll no. is ${rollno}`);
+});
 const port=3000;
 app.listen(port,() =>{
     console.log(`Server is running at http://localhost:${port}`);
